8211308719a8c26830414e5ddf181b93481d5be5
Ivan Soriano <iss@ersl.ie>
2020-08-28 15:24:56 +0200

    initial commit

---
+++ apps/tri-club-portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tri-club-suite-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tri-club-portal';
+}
